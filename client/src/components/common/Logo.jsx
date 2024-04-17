048b9f1f79d00701c8dbace5db413785a298563b
midhun04 <midhundas3110@gmail.com>
2024-04-17 12:46:37 +0530

    updated

---
+++ client/src/components/common/Logo.jsx
@@ -0,0 +1,15 @@
+import { Typography, useTheme } from '@mui/material';
+
+const Logo = () => {
+  const theme = useTheme();
+
+  return (
+    <Typography fontWeight="700" fontSize="1.7rem">
+      ⚡<span style={{ color: theme.palette.primary.main }}>Movies</span>
+    </Typography>
+  );
+};
+
+export default Logo;
+
+
